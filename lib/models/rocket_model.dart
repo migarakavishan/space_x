a997c8cd0a9d2699ad3c059f45cbee7a162e70bf
migarakavishan <migarakavishan43@gmail.com>
2024-04-03 00:36:14 +0530

    image slider image call done

--- lib/models/rocket_model.dart
+++ lib/models/rocket_model.dart
@@ -1,13 +1,19 @@
 class RocketModel {
+  String rocketID;
   String rocketName;
+  String flickrImage;
 
-  RocketModel({
-    required this.rocketName,
-  });
+  RocketModel(
+      {required this.rocketName, required this.rocketID, required this.flickrImage});
 
   factory RocketModel.fromJson(Map<String, dynamic> json) {
-    return  RocketModel(
-      rocketName: json['rocket_name']
+    return RocketModel(
+      rocketName: json['rocket_name'],
+      rocketID: json['rocket_id'],
+      flickrImage:
+          json['flickr_images'] != null && json['flickr_images'].isNotEmpty
+              ? json['flickr_images'][0]
+              : null,
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:space_x/models/rocket_model.dart';
+import 'package:space_x/services/api_service.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +13,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
     return Scaffold(
         backgroundColor: Colors.black,
         body: SafeArea(
@@ -18,7 +22,9 @@ class _HomeScreenState extends State<HomeScreen> {
             child: Column(
               children: [
                 Container(
-                  decoration: BoxDecoration(color: Colors.grey.shade900,borderRadius: BorderRadius.circular(15)),
+                  decoration: BoxDecoration(
+                      color: Colors.grey.shade900,
+                      borderRadius: BorderRadius.circular(15)),
                   child: Row(
                     children: [
                       IconButton(
@@ -38,7 +44,41 @@ class _HomeScreenState extends State<HomeScreen> {
                       const Spacer(),
                     ],
                   ),
-                )
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                FutureBuilder(
+                    future: ApiService().getAllRockets(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const CircularProgressIndicator();
+                      }
+                      List<RocketModel> rockets = snapshot.data!;
+                      return CarouselSlider(
+                        options: CarouselOptions(
+                            height: size.height * 0.25, autoPlay: true),
+                        items: rockets.map((rocket) {
+                          return Builder(
+                            builder: (BuildContext context) {
+                              return Padding(
+                                padding: const EdgeInsets.all(3.0),
+                                child: Container(
+                                  width: size.width,
+                                  decoration: BoxDecoration(
+                                      color: Colors.grey.shade900,
+                                      borderRadius: BorderRadius.circular(25),
+                                      image: DecorationImage(
+                                        fit: BoxFit.cover,
+                                          image: NetworkImage(
+                                              rocket.flickrImage))),
+                                ),
+                              );
+                            },
+                          );
+                        }).toList(),
+                      );
+                    })
               ],
             ),
           ),
