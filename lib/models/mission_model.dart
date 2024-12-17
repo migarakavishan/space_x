28a9008c5ae6fdfd7941833fa22dcbcff977fc86
migarakavishan <migarakavishan43@gmail.com>
2024-12-17 22:17:49 +0530

    `Added LaunchPadModel factory method to parse JSON data`

--- lib/models/mission_model.dart
+++ lib/models/mission_model.dart
@@ -9,4 +9,17 @@ class MissionModel {
       required this.name,
       required this.status,
       required this.location});
+
+  factory LaunchPadModel.fromJson(Map<String, dynamic> json) {
+    return LaunchPadModel(
+        id: json['id'],
+        name: json['name'],
+        status: json['status'],
+        location: json['location']['region'],
+        locationName: json['location']['name'],
+        longName: json['site_name_long'],
+        successfullaunches: json['successful_launches'],
+        attemptlaunch: json['attempted_launches'],
+        details: json['details']);
+  }
 }
